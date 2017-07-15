9b835a14aaed545ae98250aae2d5295ef211731b
Nick Heiner <nth23@cornell.edu>
2017-07-15 17:49:23 -0400

    Ok, so apparently what I want is eslint-config and not eslint-plugin

--- README.md
+++ README.md
@@ -1,36 +1,5 @@
 # eslint-plugin-nth
-
 My personal eslint config
 
-## Installation
-
-You'll first need to install [ESLint](http://eslint.org):
-
-```
-$ npm i eslint --save-dev
-```
-
-Next, install `eslint-plugin-nth`:
-
-```
-$ npm install eslint-plugin-nth --save-dev
-```
-
-**Note:** If you installed ESLint globally (using the `-g` flag) then you must also install `eslint-plugin-nth` globally.
-
 ## Usage
-
-Add `nth` to the plugins section of your `.eslintrc` configuration file. You can omit the `eslint-plugin-` prefix:
-
-```json
-{
-    "plugins": [
-        "nth"
-    ]
-}
-```
-
-
-
-
-
+[ESLint Shareable Config Docs](http://eslint.org/docs/developer-guide/shareable-configs#using-a-shareable-config)
--- index.js
+++ index.js
@@ -1,19 +1,131 @@
-/**
- * @fileoverview My personal eslint config
- * @author Nick
- */
-"use strict";
-
-//------------------------------------------------------------------------------
-// Requirements
-//------------------------------------------------------------------------------
-
-var requireIndex = require("requireindex");
-
-//------------------------------------------------------------------------------
-// Plugin Definition
-//------------------------------------------------------------------------------
-
-
-
-
+/* eslint-disable no-magic-numbers */
+module.exports = {
+  rules: {
+    'no-compare-neg-zero': 'error',
+    'no-console': 'warn',
+    'no-debugger': 'warn',
+    'no-alert': 'warn',
+    'no-empty': 'error',
+    'no-constant-condition': 'error',
+    'no-extra-boolean-cast': 'error',
+    'no-extra-semi': 'warn',
+    'no-irregular-whitespace': 'error',
+    'no-unsafe-finally': 'warn',
+    curly: 'warn',
+    'no-case-declarations': 'error',
+    'no-else-return': 'warn',
+    'no-implicit-coercion': 'error',
+    'no-magic-numbers': [
+      'warn',
+      {
+        ignoreArrayIndexes: true,
+        ignore: [
+          0,
+          1
+        ]
+      }
+    ],
+    'no-multi-spaces': 'warn',
+    'no-param-reassign': 'warn',
+    'no-return-assign': 'error',
+    'no-return-await': 'warn',
+    'no-unmodified-loop-condition': 'warn',
+    'no-useless-call': 'error',
+    'no-useless-return': 'warn',
+    'prefer-promise-reject-errors': 'warn',
+    'require-await': 'warn',
+    'array-bracket-spacing': 'warn',
+    'block-spacing': 'warn',
+    'brace-style': 'warn',
+    camelcase: 'warn',
+    'comma-dangle': 'warn',
+    'comma-spacing': 'warn',
+    'comma-style': 'warn',
+    'computed-property-spacing': 'warn',
+    'func-call-spacing': 'warn',
+    'id-blacklist': [
+      'warn',
+      'data',
+      'temp',
+      'tmp'
+    ],
+    indent: [
+      'warn',
+      2
+    ],
+    'jsx-quotes': 'warn',
+    'key-spacing': 'warn',
+    'keyword-spacing': 'warn',
+    'max-len': [
+      'warn',
+      120
+    ],
+    'no-lonely-if': 'warn',
+    'no-multiple-empty-lines': [
+      'warn',
+      {
+        max: 1
+      }
+    ],
+    'no-unneeded-ternary': 'warn',
+    'object-curly-spacing': 'warn',
+    'operator-linebreak': 'warn',
+    'quote-props': [
+      'error',
+      'as-needed'
+    ],
+    quotes: [
+      'warn',
+      'single',
+      {
+        avoidEscape: true
+      }
+    ],
+    semi: [
+      'warn',
+      'always'
+    ],
+    'semi-spacing': 'warn',
+    'space-before-blocks': 'warn',
+    'space-before-function-paren': [
+      'warn',
+      'never'
+    ],
+    'space-in-parens': 'warn',
+    'space-infix-ops': 'warn',
+    'space-unary-ops': 'warn',
+    'spaced-comment': 'warn',
+    'template-tag-spacing': 'warn',
+    'arrow-body-style': 'warn',
+    'arrow-parens': [
+      'warn',
+      'as-needed'
+    ],
+    'arrow-spacing': 'warn',
+    'generator-star-spacing': [
+      'warn',
+      {
+        before: false,
+        after: true
+      }
+    ],
+    'no-class-assign': 'error',
+    'no-var': 'error',
+    'object-shorthand': 'warn',
+    'dot-notation': 'error',
+    'no-unused-vars': 'warn',
+    'prefer-const': 'warn',
+    'prefer-rest-params': 'warn',
+    'prefer-spread': 'warn',
+    'prefer-template': 'warn',
+    'template-curly-spacing': 'warn',
+    'no-duplicate-imports': 'error',
+    'yield-star-spacing': [
+      'warn',
+      {
+        before: false,
+        after: true
+      }
+    ]
+  }
+};
